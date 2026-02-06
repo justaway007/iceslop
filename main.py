a6625dfe7cb436cc1839f9d7366d72df2e8afb0e
justaway007 <irfankarim00@gmail.com>
2026-02-06 11:07:14 +0800

    added token counts

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
-
-response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-print(response.text)
 
 def main():
-    print("Hello from iceslop!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
+    client = genai.Client(api_key=api_key)
 
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    prompt_count = response.usage_metadata.prompt_token_count
+    response_count = response.usage_metadata.candidates_token_count
+    
+    print(f"Prompt tokens: {prompt_count}")
+    print(f"Response tokens: {response_count}")
+    print(f"Response:\n{response.text}")
+    
 if __name__ == "__main__":
     main()
