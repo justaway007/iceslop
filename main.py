a2de96b4593e101d3099f0b5e7d7aa7addab3e56
justaway007 <irfankarim00@gmail.com>
2026-02-06 11:18:38 +0800

    add argument parser

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 
 def main():
@@ -9,8 +10,12 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model='gemini-2.5-flash', contents=args.user_prompt
     )
     prompt_count = response.usage_metadata.prompt_token_count
     response_count = response.usage_metadata.candidates_token_count
