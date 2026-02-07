d242bf35dbee70362e56fb7248e59f98e81282b3
justaway007 <irfankarim00@gmail.com>
2026-02-07 11:59:49 +0800

    added verbose

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
-
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -12,16 +12,22 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=args.user_prompt
+        model='gemini-2.5-flash', contents=messages
     )
+    # user_prompt = args.user_prompt
     prompt_count = response.usage_metadata.prompt_token_count
     response_count = response.usage_metadata.candidates_token_count
     
-    print(f"Prompt tokens: {prompt_count}")
-    print(f"Response tokens: {response_count}")
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_count}")
+        print(f"Response tokens: {response_count}")
+
     print(f"Response:\n{response.text}")
     
 if __name__ == "__main__":
